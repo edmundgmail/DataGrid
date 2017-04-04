9e5e3e3e506a6ff1be3bdf68159316a8c4dd95d7
eguo <eguo@t440.example.com>
2017-04-04 18:21:20 -0400

    updte

---
+++ SchedVerticle/src/main/java/com/ddp/SchedVerticle.java
@@ -0,0 +1,16 @@
+package com.ddp;
+
+/**
+ * Created by eguo on 4/4/17.
+ */
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.buffer.Buffer;
+
+public class SchedVerticle extends AbstractVerticle {
+    public void start() {
+
+    }
+}
