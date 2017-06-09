829cc31bf68520d1f98fd366fa432abe29cbb3fa
eguo <eguo@localhost.localdomain>
2017-06-08 20:10:31 -0400

    update

--- userscript.sql
+++ userscript.sql
@@ -16,32 +16,84 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `userscript`
+-- Table structure for table `report_owners`
 --
 
-DROP TABLE IF EXISTS `userscript`;
+DROP TABLE IF EXISTS `report_owners`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `userscript` (
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `cdate` datetime NOT NULL,
-  `owner` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `version` int(11) NOT NULL,
-  `content` longtext CHARACTER SET utf8 NOT NULL,
-  `filename` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `serial` smallint(6) NOT NULL,
-  PRIMARY KEY (`owner`,`name`,`version`,`filename`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE `report_owners` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `report_owner_unique_index` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `userscript`
+-- Dumping data for table `report_owners`
 --
 
-LOCK TABLES `userscript` WRITE;
-/*!40000 ALTER TABLE `userscript` DISABLE KEYS */;
-INSERT INTO `userscript` VALUES ('b','2017-06-05 10:13:37','a',1,'content','d',0),('b','2017-06-05 10:16:42','a',2,'content','d',0),('report1','2017-06-05 06:44:08','guoe2',1,'case class report1(a:String,b:String)','report.scala',1),('report2','2017-06-05 06:44:17','guoe2',1,'case class report2(a:String,b:String)','report.scala',1),('report3','2017-06-05 06:44:24','guoe2',1,'case class report3(a:String,b:String)','report.scala',1),('report1','2017-06-05 06:44:36','guoe3',1,'case class report1(a:String,b:String)','report.scala',1),('report1','2017-06-05 06:44:42','guoe4',1,'case class report1(a:String,b:String)','report.scala',1),('report2','2017-06-05 06:44:51','guoe4',1,'case class report2(a:String,b:String)','report.scala',1);
-/*!40000 ALTER TABLE `userscript` ENABLE KEYS */;
+LOCK TABLES `report_owners` WRITE;
+/*!40000 ALTER TABLE `report_owners` DISABLE KEYS */;
+INSERT INTO `report_owners` VALUES (1,'guoe2'),(2,'guoe3');
+/*!40000 ALTER TABLE `report_owners` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reports`
+--
+
+DROP TABLE IF EXISTS `reports`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reports` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner_id` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` smallint(6) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `reports_unique_index` (`owner_id`,`name`),
+  CONSTRAINT `fk_owner_id` FOREIGN KEY (`owner_id`) REFERENCES `report_owners` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reports`
+--
+
+LOCK TABLES `reports` WRITE;
+/*!40000 ALTER TABLE `reports` DISABLE KEYS */;
+INSERT INTO `reports` VALUES (1,1,'report1',0),(2,1,'report2',0),(3,2,'report3',0);
+/*!40000 ALTER TABLE `reports` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `report_files`
+--
+
+DROP TABLE IF EXISTS `report_files`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `report_files` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `report_id` int(11) DEFAULT NULL,
+  `filename` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `content` longtext COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `report_files_unique_index` (`report_id`,`filename`),
+  CONSTRAINT `fk_report_id` FOREIGN KEY (`report_id`) REFERENCES `reports` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `report_files`
+--
+
+LOCK TABLES `report_files` WRITE;
+/*!40000 ALTER TABLE `report_files` DISABLE KEYS */;
+INSERT INTO `report_files` VALUES (1,1,'file-report1.scala','aaabbbaaaaaaaaaa111'),(3,2,'file-report2.scala',''),(5,3,'file3.scala','aaaaa111 bbb555'),(9,3,'file4.scala','aaa222ttt');
+/*!40000 ALTER TABLE `report_files` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -53,4 +105,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-06-05 21:14:11
+-- Dump completed on 2017-06-08 20:09:40
